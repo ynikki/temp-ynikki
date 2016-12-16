888e8f1db440f29096b3382d42b1c5da9e380dd3
Nikki K <808nikz@gmail.com>
2016-12-15 23:58:08 -1000

    Fixing the server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+var path = require('path');
+var pug = require('pug');
 
 const PORT = 8080;
 
-app.use(express.static(__dirname + '/public'));
+const public = path.join(__dirname, 'public');
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/', express.static(public));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index');
+})
 
 app.use('/404', function (req, res) {
   res.status(404).render('404');
