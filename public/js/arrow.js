b8b34ea4ad07a38a42fb6bb394a201297f5026a3
Nikki K <808nikz@gmail.com>
2016-12-17 13:44:11 -1000

    Adding jquery to make the slides go left and right

---
+++ public/js/arrow.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function($) {
+  var sliderCount = $('#slider ul li').length;
+  var sliderWidth = $('#slider ul li').width();
+  var sliderHeight = $('#slider ul li').height();
+  var sliderUlWidth = sliderCount * sliderWidth;
+
+  $('#slider').css({ "width": "sliderUlWidth", "marginLeft": "- sliderWidth" });
+  $('#slider ul').css({ "width": "sliderUlWidth", "marginLeft": "- sliderWidth" });
+  $('#slider ul li:last-child').prependTo('#slider ul');
+
+  function goLeft() {
+    $('#slider ul').animate({ "left": "+ sliderWidth"}, 200, function() {
+      $('#slider ul li: last-child').prependTo('#slider ul');
+      $('#slider ul').css('left', '');
+    });
+  };
+
+  function goRight() {
+    $('#slider ul').animate({ "left": "- sliderWidth" }, 200, function() {
+      $('#slider ul li:first-child').appendTo('#slider ul');
+      $('#slider ul').css('left', '');
+    });
+  };
+
+  $('a.left').click(function() {
+    goLeft();
+  });
+
+  $('a.right').click(function() {
+    goRight();
+  });
+});
