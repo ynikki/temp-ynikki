572ee4fa09a3538b86c95f034ce644eb886db670
Nikki K <808nikz@gmail.com>
2016-12-23 20:52:23 -1000

    Removing the jQuery function around it

--- public/js/egghead.js
+++ public/js/egghead.js
@@ -1,18 +1,16 @@
-$(function() {
-  Snap.load("/svg/egghead.svg", function(f) {
-    var egghead = f.select("#egghead");
-    var lefteye = f.select("#left_eye");
-    var righteye = f.select("#right_eye");
- 
-    function blinkingeyes() {
-      lefteye.animate({ transform: 'scaleX(1.3, 0.1)' }, 300, mina.ease, function() {
-        lefteye.animate({ transform: 'scaleX(1, 1)' }, 300, mina.ease)
-      });
+Snap.load("/svg/egghead.svg", function(f) {
+  var egghead = f.select("#egghead");
+  var lefteye = f.select("#left_eye");
+  var righteye = f.select("#right_eye");
 
-      righteye.animate({ transform: 'scaleX(1.3, 0.1)' }, 300, mina.ease, function() {
-        righteye.animate({ transform: 'scaleX(1,1)' }, 300, mina.ease)
-      });
-    }
-    setInterval(blinkingeyes, 1000);
-  });
+  function blinkingeyes() {
+    lefteye.animate({ transform: 'scaleX(1.3, 0.1)' }, 300, mina.ease, function() {
+      lefteye.animate({ transform: 'scaleX(1, 1)' }, 300, mina.ease)
+    });
+
+    righteye.animate({ transform: 'scaleX(1.3, 0.1)' }, 300, mina.ease, function() {
+      righteye.animate({ transform: 'scaleX(1,1)' }, 300, mina.ease)
+    });
+  }
+  setInterval(blinkingeyes, 1000);
 });
