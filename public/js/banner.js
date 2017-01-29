497c1eaabe76b1e49d3d9f09cabc759ce6b643d9
Nikki K <808nikz@gmail.com>
2017-01-28 14:52:16 -1000

    Fixing the banner

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,3 +1,55 @@
 $(function() {
+  var $root = $('html');
+  var $nav_header = $('.banner');
+  var header_height = $('.banner').height();
+  var hero_height = $('.hero').height();
+  var offset_val = hero_height - header_height;
+  var eventType = ((document.ontouchstart !== null) ? 'click': 'touchstart');
+  var scroll_top = $(window).scrollTop();
+
+  function navSlide() {
+    if(scroll_top >= offset_val) {
+      $nav_header.addClass('is-sticky');
+    } else {
+      $nav_header.removeClass('is-sticky');
+    }
+  };
+
+  function menuToggle() {
+    if($nav_header.hasClass('is-open')) {
+      $root.removeClass('pinned');
+      $root.removeClass('is-open');
+    } else {
+      $root.addClass('pinned');
+      $nav_header.addClass('is-open');
+    }
+  };
+
+  function openNav() {
+    if($nav_header.hasClass('is-open')) {
+      $nav_header.removeClass('is-open');
+      $root.removeClass('pinned');
+    }
+  }
+
+  function anchorScroll(event) {
+    var id = $(this).attr('href');
+    var offset = header_height;
+    var target = $(id).offset().top - offset;
+
+    $('.banner').animate({
+      scrollTop: target
+    }, 500);
+
+    event.preventDefault();
+  };
+
+  $('.scrollto').on(eventType, function() {
+    anchorScroll.call(this, event);
+  });
+
+  $('.banner a').on(eventType, openNav);
+
+  $(window).scroll(navSlide);
 
 });
