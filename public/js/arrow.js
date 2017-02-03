d8d60244ada929715f4eac007a75f3d55aeb30a9
Nikki K <808nikz@gmail.com>
2017-02-02 23:01:38 -1000

    Commenting for now since I wanted to use a navigation slider

--- public/js/arrow.js
+++ public/js/arrow.js
@@ -1,32 +1,32 @@
-$(function() {
-  var sliderCount = $('#slider ul li').length;
-  var sliderWidth = $('#slider ul li').width();
-  var sliderHeight = $('#slider ul li').height();
-  var sliderUlWidth = sliderCount * sliderWidth;
+// $(function() {
+//   var sliderCount = $('#slider ul li').length;
+//   var sliderWidth = $('#slider ul li').width();
+//   var sliderHeight = $('#slider ul li').height();
+//   var sliderUlWidth = sliderCount * sliderWidth;
 
-  $('#slider').css({ "width": "sliderUlWidth", "marginLeft": "- sliderWidth" });
-  $('#slider ul').css({ "width": "sliderUlWidth", "marginLeft": "- sliderWidth" });
-  $('#slider ul li:first-child').prependTo('#slider ul');
+//   $('#slider').css({ "width": "sliderUlWidth", "marginLeft": "- sliderWidth" });
+//   $('#slider ul').css({ "width": "sliderUlWidth", "marginLeft": "- sliderWidth" });
+//   $('#slider ul li:first-child').prependTo('#slider ul');
 
-  function goLeft() {
-    $('#slider ul').animate({ "left": "- sliderWidth"}, 200, function() {
-      $('#slider ul li:last-child').prependTo('#slider ul');
-      $('#slider ul').css('left', '');
-    });
-  };
+//   function goLeft() {
+//     $('#slider ul').animate({ "left": "- sliderWidth"}, 200, function() {
+//       $('#slider ul li:last-child').prependTo('#slider ul');
+//       $('#slider ul').css('left', '');
+//     });
+//   };
 
-  function goRight() {
-    $('#slider ul').animate({ "left": "- sliderWidth" }, 200, function() {
-      $('#slider ul li:first-child').appendTo('#slider ul');
-      $('#slider ul').css('left', '');
-    });
-  };
+//   function goRight() {
+//     $('#slider ul').animate({ "left": "- sliderWidth" }, 200, function() {
+//       $('#slider ul li:first-child').appendTo('#slider ul');
+//       $('#slider ul').css('left', '');
+//     });
+//   };
 
-  $('a.left').click(function() {
-    goLeft();
-  });
+//   $('a.prev').click(function() {
+//     goLeft();
+//   });
 
-  $('a.right').click(function() {
-    goRight();
-  });
-});
+//   $('a.next').click(function() {
+//     goRight();
+//   });
+// });
