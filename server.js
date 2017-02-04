0cbc4a7b5a38679bd40b3224c478322f87fa2119
Nikki K <808nikz@gmail.com>
2017-02-04 00:02:55 -1000

    Adding the get for the hacc page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 var path = require('path');
 var pug = require('pug');
+var util = require('util');
+var bodyParser = require('body-parser');
 
 const PORT = 8080;
 
@@ -10,12 +12,18 @@ const public = path.join(__dirname, 'public');
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(express.static('public'));
+
 app.use('/', express.static(public));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.render('index');
-})
+});
+
+app.get('/hacc', function(req, res) {
+  res.render('hacc');
+});
 
 app.use('/404', function (req, res) {
   res.status(404).render('404');
