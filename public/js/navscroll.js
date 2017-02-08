0b73a0da5c09b97a62887f7f2a6e95a5b9f6db37
Nikki K <808nikz@gmail.com>
2017-02-07 18:04:09 -1000

    When the user is on a new page the navigation bar should be selected

--- public/js/navscroll.js
+++ public/js/navscroll.js
@@ -12,4 +12,37 @@ $(function() {
     }
   });
 
+  var main = $('#nav-center');
+  var nav = $('nav');
+  var nav_height = nav.outerHeight();
+
+  $(window).on('scroll', function() {
+    var cur_pos = $(this).scrollTop();
+
+    main.each(function() {
+      var top = $(this).offset().top - nav_height;
+      var bottom = top + $(this).outerHeight();
+
+      if(cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('center-active');
+
+        main.removeClass('center-active');
+        // $(this).addClass('center-active');
+
+        nav.find('a[href="/'+ location.pathname.split("/")[1] +'"]').addClass('center-active');
+      }
+    });
+  });
+
+  nav.find('a').on('click', function() {
+    var $el = $(this);
+    var id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - nav_height
+    }, 500)
+
+    return false;
+  });
+
 });
